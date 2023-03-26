5b0f3ef6a6e64f5df21a695e5915947186999854
BONNe <bonne@bonne.id.lv>
2023-03-26 14:05:52 +0300

    Update to new Bukkit Loader

--- src/main/java/world/bentobox/farmersdance/FarmersDancePladdon.java
+++ src/main/java/world/bentobox/farmersdance/FarmersDancePladdon.java
@@ -7,7 +7,6 @@
 package world.bentobox.farmersdance;
 
 
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
 import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
@@ -19,9 +18,8 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  * This is a dummy class that allows to load addon via Bukkit Plugin loader.
  * @author BONNe
  */
-@Plugin(name="Pladdon", version="1.0")
+@Plugin(name="FarmersDance", version="1.0")
 @ApiVersion(ApiVersion.Target.v1_17)
-@Dependency(value = "BentoBox")
 public class FarmersDancePladdon extends Pladdon
 {
     @Override
